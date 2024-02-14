f9a1666775ed365a41408abc1eded7f17054f222
Mohan Patil <mohan.patil@hyniva.com>
2024-02-14 15:32:54 +0530

    new changes for validation

--- src/Lamda/employee/employeeHandler.js
+++ src/Lamda/employee/employeeHandler.js
@@ -1,20 +1,12 @@
-const {
-  DynamoDBClient,
-  PutItemCommand,
-  UpdateItemCommand,
-  DeleteItemCommand,
-  GetItemCommand,
-  ScanCommand,
-} = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, PutItemCommand, UpdateItemCommand, DeleteItemCommand, GetItemCommand, ScanCommand } = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 const moment = require("moment");
 const client = new DynamoDBClient();
-const {
-  httpStatusCodes,
-  httpStatusMessages,
-} = require("../../environment/appconfig");
+const { validateEmployeeDetails, validateUpdateEmployeeDetails } = require("../../validator/validateRequest");
+const { updateEmployeeAllowedFields } = require("../../validator/validateFields");
+const { httpStatusCodes, httpStatusMessages } = require("../../environment/appconfig");
 const currentDate = Date.now(); // get the current date and time in milliseconds
-const formattedDate = moment(currentDate).format("YYYY-MM-DD HH:mm:ss"); //formating date
+const formattedDate = moment(currentDate).format("MM-DD-YYYY HH:mm:ss"); //formating date
 
 const createEmployee = async (event) => {
   console.log("Create employee details");
@@ -23,17 +15,25 @@ const createEmployee = async (event) => {
     const requestBody = JSON.parse(event.body);
 
     // Check for required fields
-    const requiredFields = [
-      "employeeId",
-      "firstName",
-      "lastName",
-      "dateOfBirth",
-      "officeEmailAddress",
-      "branchOffice",
-    ];
-    if (!requiredFields.every((field) => requestBody[field])) {
+    if (!validateEmployeeDetails(requestBody)) {
       throw new Error("Required fields are missing.");
     }
+
+    // Check if the employeeId already exists
+    const employeeIdExists = await isEmployeeIdExists(requestBody.employeeId);
+    if (employeeIdExists) {
+      throw new Error("EmployeeId already exists.");
+    }
+
+    // Check if the email address already exists
+    const emailExists = await isEmailExists(requestBody.officeEmailAddress);
+    if (emailExists) {
+      throw new Error("Email address already exists.");
+    }
+
+    // Fetch the highest highestSerialNumber from the DynamoDB table
+    // const highestSerialNumber = await getHighestSerialNumber();
+    // const nextSerialNumber = highestSerialNumber !== undefined ? highestSerialNumber + 1 : 1;
 
     const params = {
       TableName: process.env.EMPLOYEE_TABLE,
@@ -43,7 +43,7 @@ const createEmployee = async (event) => {
         lastName: requestBody.lastName,
         dateOfBirth: requestBody.dateOfBirth,
         officeEmailAddress: requestBody.officeEmailAddress,
-        //branchOffice: requestBody.branchOffice,
+        branchOffice: requestBody.branchOffice,
         password: requestBody.password || null,
         gender: requestBody.gender || null,
         ssnNumber: requestBody.ssnNumber || null,
@@ -58,7 +58,7 @@ const createEmployee = async (event) => {
         contactNumber: requestBody.contactNumber || null,
         joiningDate: requestBody.joiningDate || null,
         emergencyContactPerson: requestBody.emergencyContactPerson || null,
-        //designation: requestBody.designation || null,
+        designation: requestBody.designation || null,
         emergencyContactNumber: requestBody.emergencyContactNumber || null,
         resignedDate: requestBody.resignedDate || null,
         relievedDate: requestBody.relievedDate || null,
@@ -66,6 +66,7 @@ const createEmployee = async (event) => {
         createdDateTime: formattedDate,
         updatedDateTime: requestBody.updatedDateTime || null,
         department: requestBody.department || null,
+        aadhaarNumber: requestBody.aadhaarNumber || null,
       }),
     };
     const createResult = await client.send(new PutItemCommand(params));
@@ -139,10 +140,9 @@ const createEmployee = async (event) => {
     };
 
     const createAssignmentResult = await client.send(new PutItemCommand(assignmentParams));
-  
     response.body = JSON.stringify({
-      message: httpStatusMessages.SUCCESSFULLY_CREATED_EMPLOYEE_DETAIL,
-      createEmployee,
+      message: httpStatusMessages.SUCCESSFULLY_CREATED_EMPLOYEE_DETAILS,
+      createResult,
     });
   } catch (e) {
     console.error(e);
@@ -156,45 +156,131 @@ const createEmployee = async (event) => {
   return response;
 };
 
-const getEmployee = async (event) => {
-  console.log("Get employee details");
+const updateEmployee = async (event) => {
+  console.log("Update employee details");
   const response = { statusCode: httpStatusCodes.SUCCESS };
+
   try {
-    const { pathParameters } = event;
-    if (!pathParameters || !pathParameters.employeeId) {
-      console.log("employeeId is missing");
-      throw {
-        statusCode: httpStatusCodes.BAD_REQUEST,
-        message: httpStatusMessages.EMPLOYEE_ID_REQUIRED,
-      };
+    const requestBody = JSON.parse(event.body);
+    console.log("Request Body:", requestBody);
+    const currentDate = Date.now(); 
+    const formattedDate = moment(currentDate).format("MM-DD-YYYY HH:mm:ss");
+    const employeeId = event.pathParameters ? event.pathParameters.employeeId : null;
+    if (!employeeId) {
+      console.log("Employee Id is required");
+      throw new Error(httpStatusMessages.EMPLOYEE_ID_REQUIRED);
+    }
+
+    const getItemParams = {
+      TableName: process.env.EMPLOYEE_TABLE,
+      Key: marshall({ employeeId }),
+    };
+    const { Item } = await client.send(new GetItemCommand(getItemParams));
+    if (!Item) {
+      console.log(`Employee with employeeId ${employeeId} not found`);
+      response.statusCode = 404;
+      response.body = JSON.stringify({
+        message: `Employee with employeeId ${employeeId} not found`,
+      });
+      return response;
+    }
+
+    requestBody.updatedDateTime = formattedDate;
+
+    const objKeys = Object.keys(requestBody).filter((key) => updateEmployeeAllowedFields.includes(key));
+    console.log(`Employee with objKeys ${objKeys} `);
+    const validationResponse = validateUpdateEmployeeDetails(requestBody);
+    console.log(`valdation : ${validationResponse.validation} message: ${validationResponse.validationMessage} `);
+
+    if (!validationResponse.validation) {
+      console.log(validationResponse.validationMessage);
+      response.statusCode = 400;
+      response.body = JSON.stringify({
+        message: validationResponse.validationMessage,
+      });
+      return response;
+    }
+
+    const officeEmailAddressExists = await isEmailNotEmployeeIdExists(requestBody.officeEmailAddress, employeeId);
+    if (officeEmailAddressExists) {
+      console.log("officeEmailAddress already exists.");
+      response.statusCode = 400;
+      response.body = JSON.stringify({
+        message: "officeEmailAddress already exists.",
+      });
+      return response;
     }
 
     const params = {
       TableName: process.env.EMPLOYEE_TABLE,
-      Key: marshall({ employeeId: pathParameters.employeeId }),
+      Key: marshall({ employeeId }),
+      UpdateExpression: `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
+      ExpressionAttributeNames: objKeys.reduce(
+        (acc, key, index) => ({
+          ...acc,
+          [`#key${index}`]: key,
+        }),
+        {}
+      ),
+      ExpressionAttributeValues: marshall(
+        objKeys.reduce(
+          (acc, key, index) => ({
+            ...acc,
+            [`:value${index}`]: requestBody[key],
+          }),
+          {}
+        )
+      ),
+      ":updatedDateTime": requestBody.updatedDateTime,
     };
 
-    const { employeeDetails } = await client.send(new GetItemCommand(params));
-    console.log("Employee Details: " + { employeeDetails });
-    if (!employeeDetails) {
-      console.log("No Employee Detatils found");
-      throw {
-        statusCode: httpStatusCodes.NOT_FOUND,
+    const updateResult = await client.send(new UpdateItemCommand(params));
+    console.log("Successfully updated Employee details.");
+    response.body = JSON.stringify({
+      message: httpStatusMessages.SUCCESSFULLY_UPDATED_EMPLOYEE_DETAILS,
+      employeeId: employeeId,
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 400;
+    response.body = JSON.stringify({
+      message: httpStatusMessages.FAILED_TO_UPDATED_EMPLOYEE_DETAILS,
+      employeeId: requestBody.employeeId, // If you want to include employeeId in the response
+      errorMsg: e.message,
+    });
+  }
+  return response;
+};
+
+const getEmployee = async (event) => {
+  console.log("Get employee details");
+  const response = { statusCode: httpStatusCodes.SUCCESS };
+  try {
+    const params = {
+      TableName: process.env.EMPLOYEE_TABLE,
+      Key: marshall({ employeeId: event.pathParameters.employeeId }),
+    };
+    const { Item } = await client.send(new GetItemCommand(params));
+    console.log({ Item });
+    if (!Item) {
+      console.log("Employee details not found.");
+      response.statusCode = httpStatusCodes.NOT_FOUND;
+      response.body = JSON.stringify({
         message: httpStatusMessages.EMPLOYEE_DETAILS_NOT_FOUND,
-      };
+      });
     } else {
+      console.log("Successfully retrieved Employee details.");
       response.body = JSON.stringify({
         message: httpStatusMessages.SUCCESSFULLY_RETRIEVED_EMPLOYEE_DETAILS,
-        data: unmarshall(employeeDetails),
+        data: unmarshall(Item),
       });
-      console.log("Employee Detatils" + response);
     }
   } catch (e) {
     console.error(e);
     response.body = JSON.stringify({
-      statusCode: e.statusCode || httpStatusCodes.INTERNAL_SERVER_ERROR,
-      message:
-        e.message || httpStatusMessages.FAILED_TO_RETRIEVE_EMPLOYEE_DETAILS,
+      statusCode: e.statusCode,
+      message: httpStatusMessages.FAILED_TO_RETRIEVE_EMPLOYEE_DETAILS,
+      errorMsg: e.message,
     });
   }
   return response;
@@ -203,9 +289,7 @@ const getEmployee = async (event) => {
 const getAllEmployees = async () => {
   const response = { statusCode: httpStatusCodes.SUCCESS };
   try {
-    const { Items } = await client.send(
-      new ScanCommand({ TableName: process.env.EMPLOYEE_TABLE })
-    ); // Getting table name from the servetless.yml and setting to the TableName
+    const { Items } = await client.send(new ScanCommand({ TableName: process.env.EMPLOYEE_TABLE })); // Getting table name from the servetless.yml and setting to the TableName
 
     if (Items.length === 0) {
       // If there is no employee details found
@@ -214,9 +298,7 @@ const getAllEmployees = async () => {
         message: httpStatusMessages.EMPLOYEE_DETAILS_NOT_FOUND,
       }); // Setting error message
     } else {
-      const sortedItems = Items.sort((a, b) =>
-        a.employeeId.S.localeCompare(b.employeeId.S)
-      );
+      const sortedItems = Items.sort((a, b) => a.employeeId.S.localeCompare(b.employeeId.S));
 
       // Map and set "password" field to null
       const employeesData = sortedItems.map((item) => {
@@ -243,8 +325,73 @@ const getAllEmployees = async () => {
   return response;
 };
 
+// Function to check if employeeId already exists
+const isEmployeeIdExists = async (employeeId) => {
+  const params = {
+    TableName: process.env.EMPLOYEE_TABLE,
+    Key: { employeeId: { S: employeeId } },
+  };
+  const { Item } = await client.send(new GetItemCommand(params));
+  // If Item is not null, employeeId exists
+  return !!Item;
+};
+
+const isEmailExists = async (emailAddress) => {
+  const params = {
+    TableName: process.env.EMPLOYEE_TABLE,
+    FilterExpression: "officeEmailAddress = :email",
+    ExpressionAttributeValues: {
+      ":email": { S: emailAddress },
+    },
+    ProjectionExpression: "officeEmailAddress",
+  };
+
+  const command = new ScanCommand(params);
+  const data = await client.send(command);
+  return data.Items.length > 0;
+};
+
+const isEmailNotEmployeeIdExists = async (emailAddress, employeeId) => {
+  console.log("in side isEmailNotEmployeeIdExists");
+  const params = {
+    TableName: process.env.EMPLOYEE_TABLE,
+    FilterExpression: "officeEmailAddress = :email AND employeeId <> :id",
+    ExpressionAttributeValues: {
+      ":email": { S: emailAddress },
+      ":id": { S: employeeId }, // Assuming employeeId is a string, adjust if needed
+    },
+    ProjectionExpression: "officeEmailAddress",
+  };
+  const command = new ScanCommand(params);
+  const data = await client.send(command);
+  return data.Items.length > 0;
+};
+
+// async function getHighestSerialNumber() {
+//   const params = {
+//     TableName: process.env.EMPLOYEE_TABLE,
+//     ProjectionExpression: 'serialNumber',
+//     Limit: 1,
+//     ScanIndexForward: false, // Sort in descending order to get the highest serial number first
+//   };
+
+//   try {
+//     const result = await client.send(new ScanCommand(params));
+//     if (result.Items.length === 0) {
+//       return undefined; // If no records found, return undefined
+//     } else {
+//       // Parse and return the highest serial number without incrementing
+//       return parseInt(result.Items[0].serialNumber.N);
+//     }
+//   } catch (error) {
+//     console.error("Error retrieving highest serial number:", error);
+//     throw error; // Propagate the error up the call stack
+//   }
+// }
+
 module.exports = {
   createEmployee,
+  updateEmployee,
   getEmployee,
   getAllEmployees,
 };
